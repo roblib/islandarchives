0a6e5ce1ef5c654fcd827b55b657f177f5853445
Rob Drew <rdrew@upei.ca>
2020-03-03 08:47:13 -0400

    .

--- template.php
+++ template.php
@@ -28,8 +28,8 @@ function islandarchives_form_alter(&$form, &$form_state, $form_id) {
 	// target a single form
 	if($form_id == "islandora_solr_simple_search_form"){
 		$form['simple']['islandora_simple_search_query']['#default_value'] = t('Search the archives...'); // Set a default value for the textfield
-		$form['simple']['islandora_simple_search_query']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search IslandScholar:';}";
-		$form['simple']['islandora_simple_search_query']['#attributes']['onfocus'] = "if (this.value == 'Search IslandScholar:') {this.value = '';}";
+		$form['simple']['islandora_simple_search_query']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search the archives...';}";
+		$form['simple']['islandora_simple_search_query']['#attributes']['onfocus'] = "if (this.value == 'Search the archives...') {this.value = '';}";
 	}
 }
 

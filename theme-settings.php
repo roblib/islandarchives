7652142d167c6b05023e041dfbf384247dd27559
rdrew <rdrew@upei.ca>
2020-07-16 10:45:53 -0300

    more cosmetics

--- theme-settings.php
+++ theme-settings.php
@@ -6,45 +6,45 @@
  */
 
 require_once dirname(__FILE__) . '/template.php';
-
 /**
- * Implements hook_form_FORM_alter().
+ * Implementation of hook_settings() for themes.
  */
-//function islandarchives_form_system_theme_settings_alter(&$form, $form_state) {
-  //$default_file_dir = 'public://';
-  //$folder = file_prepare_directory($default_file_dir, FILE_CREATE_DIRECTORY);
-  //$settings_theme = $form_state['build_info']['args'][0];
+function islandarchives_settings($settings) {
+  // This ensures that a 'files' directory exists if it hasn't
+  // already been been created.
+  file_check_directory(file_directory_path(),
+    FILE_CREATE_DIRECTORY, 'file_directory_path');
 
-  //if ($folder) {
-    //$header_icons = theme_get_setting('header_icons');
-    //// BUG: Force file to be permanent.
-    //if (!empty($header_icons)) {
-      //_fix_permanent_image('header_icons', $settings_theme);
-    //}
-    //$form['header_icons'] = array(
-      //'#type'               => 'managed_file',
-      //'#title'              => t('Header Icons'),
-      //'#description' => t("Image should be SVG format."),
-      //'#default_value'      => $header_icons,
-      //'#progress_message'   => t('Please wait...'),
-      //'#progress_indicator' => 'bar',
-      //'#upload_location'    => $default_file_dir,
-      //'#upload_validators'  => array(
-        //'file_validate_extensions' => array('svg')
-      //),
-    //);
-  //}
-//}
+  // Check for a freshly uploaded header image, save it to the
+  // filesystem, and grab its full path for later use.
+  if ($file = file_save_upload('header_image',
+      array('file_validate_is_image' => array()))) {
+    $parts = pathinfo($file->filename);
+    $filename = 'islandarchives_header_image.'. $parts['extension'];
+    if (file_copy($file, $filename, FILE_EXISTS_REPLACE)) {
+      $settings['header_image_path'] = $file->filepath;
+    }
+  }
 
-//function _fix_permanent_image($image, $theme) {
-  //$fid = theme_get_setting($image, $theme);
-  //if ($fid > 0) {
-    //$file = file_load($fid);
-    //if (is_object($file) && $file->status == 0) {
-      //$file->status = FILE_STATUS_PERMANENT;
-      //file_save($file);
-      //file_usage_add($file, $theme, 'theme', 1);
-      //drupal_set_message($image . ' saved permanently.', 'status');
-    //}
-  //}
-//}
+  // Define the settings-related FormAPI elements.
+  $form = array();
+  $form['header_image'] = array(
+    '#type' => 'file',
+    '#title' => t('Header image'),
+    '#maxlength' => 40,
+  );
+  $form['header_image_path'] = array(
+    '#type' => 'value',
+    '#value' => !empty($settings['header_image_path']) ?
+      $settings['header_image_path'] : '',
+  );
+  if (!empty($settings['header_image_path'])) {
+    $form['header_image_preview'] = array(
+      '#type' => 'markup',
+      '#value' => !empty($settings['header_image_path']) ?
+		  theme('image', $settings['header_image_path']) : '',
+    );
+  }
+  return $form;
+}
+
